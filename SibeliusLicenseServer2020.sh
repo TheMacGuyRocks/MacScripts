112705cf2893752b74905a55d96f082675bd9b3e
The Mac Guy <54036482+TheMacGuyRocks@users.noreply.github.com>
2020-07-04 17:49:07 +0100

    Create SibeliusLicenseServer2020.sh

---
+++ SibeliusLicenseServer2020.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#hostname="mysibelius.mydomain.domain"
+hostname="Your Sibelius License Server FQDN"
+
+mkdir -p "/Library/Application Support/Avid/Sibelius/_manuscript"
+
+touch "/Library/Application Support/Avid/Sibelius/_manuscript/LicenceServerInfo"
+
+echo "$hostname:7312">"/Library/Application Support/Avid/Sibelius/_manuscript/LicenceServerInfo"
+sudo defaults write /Library/LaunchAgents/com.avid.ApplicationManager.plist RunAtLoad -boolean false
