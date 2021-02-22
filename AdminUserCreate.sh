5ed1291ce51ed933ac65184ac15adca3ef7b4034
The Mac Guy <54036482+TheMacGuyRocks@users.noreply.github.com>
2021-02-22 10:00:30 +0000

    With Sudos

--- AdminUserCreate.sh
+++ AdminUserCreate.sh
@@ -8,13 +8,13 @@
 MAXID=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -ug | tail -1)
 USERID=$((MAXID+1))
 
-dscl . -create /Users/$username
-dscl . -create /Users/$username UserShell /bin/bash
-dscl . -create /Users/$username RealName $username 
-dscl . -create /Users/$username UniqueID $USERID
-dscl . -create /Users/$username PrimaryGroupID 20
-dscl . -create /Users/$username NFSHomeDirectory /Users/$username
-dscl . -passwd /Users/$username $password 
+sudo dscl . -create /Users/$username
+sudo dscl . -create /Users/$username UserShell /bin/bash
+sudo dscl . -create /Users/$username RealName $username 
+sudo dscl . -create /Users/$username UniqueID $USERID
+sudo dscl . -create /Users/$username PrimaryGroupID 20
+sudo dscl . -create /Users/$username NFSHomeDirectory /Users/$username
+sudo dscl . -passwd /Users/$username $password 
 
 #Comment to make make standard user
-dscl . -append /Groups/admin GroupMembership $username
+sudo dscl . -append /Groups/admin GroupMembership $username
