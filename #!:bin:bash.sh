ae99be1bc6af92b978392cbf029e1248c34c92b7
The Mac Guy <54036482+TheMacGuyRocks@users.noreply.github.com>
2021-07-23 11:49:26 +0100

    Create #!:bin:bash.sh
    New File to reset audio privacy and Logic/GB Preferences.

---
+++ #!:bin:bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# -----------------------  Reset Sound Privacy ----------------------- #
+# ------------ Created by TheMacGuy - tom@themacguy.rocks ------------ #
+
+tccutil reset Microphone
+rm -rf ~/Library/Preferences/com.apple.logic.pro.cs
+rm -rf ~/Library/Preferences/com.apple.logic10.cs
+rm -rf ~/Library/Preferences/com.apple.logic.pro.plist
+rm -rf ~/Library/Preferences/Logic
