508b7dda7b48fbcdcada606ff7d23a7e723efce3
The Mac Guy <54036482+TheMacGuyRocks@users.noreply.github.com>
2021-08-27 23:42:02 +0100

    General Cleanup, additional Notes

--- SMB/mount-network-home.sh
+++ SMB/mount-network-home.sh
@@ -12,14 +12,20 @@ domainName=$(echo "show com.apple.opendirectoryd.ActiveDirectory" | scutil | gre
 
 # Read the user network home folder from Active Directory
 myHomeFolder=$(/usr/bin/dscl localhost -read /Active\ Directory/$domainName/All\ Domains/Users/$theUserName SMBHome | cut -d':' -f2-  | tr '\' '/' |  tr '\n' ' ' | cut -d' ' -f2- ) 
+
+# Replace Spaces with %20
 noSpaces=$(echo $myHomeFolder | sed 's/ /%20/')
 
+#The Variable using th eno spaces result
 myHomeFolderSMB=$(/bin/echo "$noSpaces")
 
+#The location of Mysides
 MYSIDES="/usr/local/bin/mysides"
 
+#Get check if the user is a local user.
 dscl /Local/Default -list Users | grep $USER &> /dev/null
 
+#IF They are a local user
 if [ $? == 0 ]; then
     echo "Local User, skipping folder Run"
 	MYSIDES remove all #Clears all items in the sidebar
@@ -30,11 +36,13 @@ if [ $? == 0 ]; then
 	MYSIDES add Movies file:///Users/$USER/Movies #Adds Movies
 	MYSIDES add Applications file:///Applications #Adds Applications
 else
-	# Keep trying until the network home is mounted
+#If they are not a local user.
+
+	# Try the following until the network home is mounted
 	while [[ ! -d ~/MyNetworkHome/Documents ]]; do
 
 	echo "Sleeping"
-	/bin/sleep 5
+	/bin/sleep 1
 
 	# Unlink any existing symbolic link and create the mount location folder if necessary
 	/bin/unlink ~/MyNetworkHome
@@ -43,56 +51,32 @@ else
 	# Mount the network home folder
 	/sbin/mount_smbfs $myHomeFolderSMB /Users/$theUserName/MyNetworkHome
 
-	# If the relevant directories exist then create the symbolic links
-	if [ -d ~/MyNetworkHome/Documents ] && [ -d ~/Documents ]; then
-		/bin/unlink ~/Documents/Network\ Documents
-		/bin/ln -s ~/MyNetworkHome/Documents  ~/Documents/Network\ Documents
-	else
+	# Create the symbolic links
 		mkdir ~/MyNetworkHome/Documents
 		/bin/unlink ~/Documents/Network\ Documents
 		/bin/ln -s ~/MyNetworkHome/Documents  ~/Documents/Network\ Documents
-	fi
 
-	if [ -d ~/MyNetworkHome/Movies ] && [ -d ~/Movies ]; then
-		/bin/unlink  ~/Movies/Network\ Movies
-		/bin/ln -s ~/MyNetworkHome/Movies ~/Movies/Network\ Movies
-	else
 		mkdir ~/MyNetworkHome/Movies
 		/bin/unlink  ~/Movies/Network\ Movies
 		/bin/ln -s ~/MyNetworkHome/Movies ~/Movies/Network\ Movies
-	fi
 
-	if [ -d ~/MyNetworkHome/Music ] && [ -d ~/Music ]; then
-		/bin/unlink ~/Music/Network\ Music
-		/bin/ln -s ~/MyNetworkHome/Music  ~/Music/Network\ Music
-	else
 		mkdir ~/MyNetworkHome/Music
 		/bin/unlink ~/Music/Network\ Music
 		/bin/ln -s ~/MyNetworkHome/Music  ~/Music/Network\ Music
-	fi
 
-	if [ -d ~/MyNetworkHome/Pictures ] && [ -d ~/Pictures ]; then
-		/bin/unlink ~/Pictures/Network\ Pictures
-		/bin/ln -s ~/MyNetworkHome/Pictures  ~/Pictures/Network\ Pictures
-	else
 		mkdir ~/MyNetworkHome/Pictures
 		/bin/unlink ~/Pictures/Network\ Pictures
 		/bin/ln -s ~/MyNetworkHome/Pictures  ~/Pictures/Network\ Pictures
-	fi
 
-	if [ -d ~/MyNetworkHome/Desktop ] && [ -d ~/Desktop ]; then
-		/bin/unlink ~/Pictures/Network\ Desktop
-		/bin/ln -s ~/MyNetworkHome/Desktop  ~/Pictures/Network\ Desktop
-	else
 		mkdir ~/MyNetworkHome/Desktop
 		/bin/unlink ~/Pictures/Network\ Desktop
 		/bin/ln -s ~/MyNetworkHome/Desktop  ~/Pictures/Network\ Desktop
-	fi
 
-	#/bin/ln -s ~/MyNetworkHome ~/Desktop/Network\ Home
+		/bin/ln -s ~/MyNetworkHome ~/Desktop/Network\ Home
 
+	done
 		echo "Sleeping"
-		sleep 5
+		sleep 2
 		MYSIDES remove all #Clears all items in the sidebar
 		MYSIDES add Home file:///Users/$theUserName/MyNetworkHome #Adds User Home
 		MYSIDES add Documents file:///Users/$theUserName/MyNetworkHome/Documents #Adds Documents
@@ -100,7 +84,11 @@ else
 		MYSIDES add Pictures file:///Users/$theUserName/MyNetworkHome/Pictures #Adds Pictures
 		MYSIDES add Applications file:///Applications #Adds Applications
 
-	done
+		touch ~/DO_NOT_SAVE_HERE.txt
+		touch ~/Documents/DO_NOT_SAVE_HERE.txt
+		touch ~/Pictures/DO_NOT_SAVE_HERE.txt
+		touch ~/Music/DO_NOT_SAVE_HERE.txt
+		
 fi
 
 exit 0
